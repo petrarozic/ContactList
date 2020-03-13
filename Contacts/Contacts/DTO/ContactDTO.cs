e1ea51becf075f5882ee45755bd57f0be2c93ccc
unknown <petra.rozic01@gmail.com>
2020-03-13 18:40:18 +0100

    Defined column titles

--- Contacts/Contacts/DTO/ContactDTO.cs
+++ Contacts/Contacts/DTO/ContactDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,12 +10,16 @@ namespace Contacts.DTO
     {
         public int ContactId { get; set; }
 
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
+        [Display(Name = "City")]
         public string City { get; set; }
         public string Note { get; set; }
 
         public List<PhoneNumberDTO> PhoneNumbers { get; set; }
+        [Display(Name = "Phone numbers")]
         public string PhoneNumbersString { get; set; }
     }
 }
